55fa162cf6665f4d6512721d8d8e4f8b9e9d1d1c
codewithdarkwa <jnrdrkw1@gmail.com>
2022-08-06 17:57:17 +0000

    refactor app.json

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Home from './Home'
 export default function App() {
   return (
+    <>
     <Home />
+    <StatusBar style='dark'/>
+    </>
   );
 }
 
